fbc360870190481191e8d94eb0170885e4dfd719
Hanz Christian Angelo G Magbal <hanzhmagbal@gmail.com>
2026-02-04 07:11:21 +0000

    ## Error Type Console Error
    ## Error Message
    A tree hydrated but some a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,12 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&family=Space+Mono:wght@400;700&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet"/>
-      </head>
+      <head />
       <body className="font-display bg-background text-foreground selection:bg-primary selection:text-white antialiased">
           <ThemeProvider
             defaultTheme="dark"
